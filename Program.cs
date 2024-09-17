400777ef40fe496ce51713fb78df18cbb279469d
Korkken <Korkken@gmail.com>
2024-09-17 15:34:33 +0200

    Added Methods and if the player wants to go for another round after game is over

--- Program.cs
+++ Program.cs
@@ -2,32 +2,49 @@
 
 
 Random random = new Random();
-int lives = 5;
+int lives;
 int randomNumber = random.Next(1, 21);
-Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
-while (lives > 0)
+
+MainGame();
+void MainGame()
 {
-    
-    int.TryParse(Console.ReadLine(), out int guessedNumber);
-    if (guessedNumber == randomNumber)
+    lives = 5;
+    Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
+    while (lives > 0) //Continue the game until the person runs out of guesses/lives
     {
-        Console.WriteLine("Wohoo! Du klarade det!");
-        
+
+        int.TryParse(Console.ReadLine(), out int guessedNumber);
+        CheckGuess(guessedNumber);
+
     }
-    else if (guessedNumber < randomNumber && lives > 0)
+}
+void CheckGuess(int guessedNumber) //Check guessed number against randomed number to see if the player has guessed correctly
+{
+    if (guessedNumber == randomNumber) //Player guessed correctly
+    {
+        Console.WriteLine("Wohoo! Du klarade det!");  
+
+    }
+    else if (guessedNumber < randomNumber && lives > 0) //Player guessed a number that was lower than the randomed number
     {
         Console.WriteLine("Tyvärr, du gissade för lågt!");
         lives--;
-        
+
     }
-    else if (guessedNumber > randomNumber && lives > 0)
+    else if (guessedNumber > randomNumber && lives > 0) //Player guessed a number that was higher than the randomed number
     {
         Console.WriteLine("Tyvärr, du gissade för högt!");
         lives--;
-        
+
     }
-
 }
-if ( lives== 0) { Console.WriteLine("Tyvärr, du lyckades inte gissa talet på fem försök!"); }
+if (lives == 0) { Console.WriteLine("Tyvärr, du lyckades inte gissa talet på fem försök!"); } //Player ran out of lives
 
-Console.ReadLine();
+Console.WriteLine("Skulle du vilja testa igen? ja eller nej?");
+string rematch = Console.ReadLine();
+if (rematch.ToLower() == "ja")
+{
+    MainGame();
+}
+else { Console.WriteLine("Jahapp, det var ju tråkigt för dig"); }
+
